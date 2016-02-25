3760313dca2f0ba2fafc55766108d27e298ac980
Christoph Papke <chrisipa@users.noreply.github.com>
2016-02-25 13:16:29 +0100

    Added support for docker-compose, mysql and postgres databases

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,10 @@
 FROM sonarqube
 MAINTAINER Christoph Papke <info@christoph-papke.de>
 
+# install additional packages
+RUN apt-get update && \
+	apt-get install -y netcat
+
 # copy entry point to docker image root
 COPY docker-entrypoint.sh /entrypoint.sh
 
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,17 +1,85 @@
 #!/bin/bash
 set -e
 
-if [ "${1:0:1}" != '-' ]; then
-	exec "$@"
+if [ -z $DB_TYPE ]
+then
+	DB_TYPE="mysql"
 fi
+
+if [ -z $DB_HOST ]
+then
+	if [ -n "$MYSQL_PORT_3306_TCP_ADDR" ]
+	then
+		DB_HOST="$MYSQL_PORT_3306_TCP_ADDR"
+	else 
+		DB_HOST="pwsafe-mysql"
+	fi
+fi
+
+if [ -z $DB_PORT ]
+then
+	if [ -n "$MYSQL_PORT_3306_TCP_PORT" ]
+	then 
+		DB_PORT="$MYSQL_PORT_3306_TCP_PORT"
+	else
+		DB_PORT="3306"
+	fi 
+fi
+
+if [ -z $DB_NAME ]
+then 
+	if [ -n "$MYSQL_ENV_MYSQL_DATABASE" ]
+	then
+		DB_NAME="$MYSQL_ENV_MYSQL_DATABASE"
+	else
+		DB_NAME="pwsafe"
+	fi
+fi
+
+if [ -z $DB_USER ]
+then 
+	if [ -n "$MYSQL_ENV_MYSQL_USER" ]
+	then
+		DB_USER="$MYSQL_ENV_MYSQL_USER"
+	else
+		DB_USER="pwsafe"
+	fi
+fi
+
+if [ -z $DB_PASS ]
+then 
+	if [ -n "$MYSQL_ENV_MYSQL_PASSWORD" ]
+	then
+		DB_PASS="$MYSQL_ENV_MYSQL_PASSWORD"
+	else
+		DB_PASS="my-password"
+	fi
+fi
+
+# set db dialect and db driver class name by db type
+case "$DB_TYPE" in
+	mysql)
+		DB_URL="jdbc:mysql://$DB_HOST:$DB_PORT/$DB_NAME?useUnicode=true&characterEncoding=utf8"
+	;;
+	postgresql)
+		DB_URL="jdbc:postgresql://$DB_HOST:$DB_PORT/$DB_NAME"
+	;;
+	*)
+		echo "ERROR: The db type '$DB_TYPE' is not supported"
+		exit 1
+	;;
+esac
+
+# wait for database port to be opened
+while ! nc -z $DB_HOST $DB_PORT; do sleep 1; done
 
 exec java -jar lib/sonar-application-$SONAR_VERSION.jar \
   -Dsonar.log.console=true \
-  -Dsonar.jdbc.username="$SONARQUBE_JDBC_USERNAME" \
-  -Dsonar.jdbc.password="$SONARQUBE_JDBC_PASSWORD" \
-  -Dsonar.jdbc.url="$SONARQUBE_JDBC_URL" \
+  -Dsonar.jdbc.username="$DB_USER" \
+  -Dsonar.jdbc.password="$DB_PASS" \
+  -Dsonar.jdbc.url="$DB_URL" \
   -Dsonar.web.context="$SONARQUBE_CONTEXT_PATH" \
-  -Dsonar.web.javaAdditionalOpts="-Djava.security.egd=file:/dev/./urandom" \
+  -Dsonar.web.javaAdditionalOpts="$SONARQUBE_JAVA_OPTS -Djava.security.egd=file:/dev/./urandom" \
 	"$@"
 
 exec "$@"
